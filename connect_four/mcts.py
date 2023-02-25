219b58f78bb3d187ec067c9407d45025ce762f4e
zfac131 <zfac131@live.rhul.ac.uk>
2023-02-25 18:22:14 +0000

    revert mcts.py

--- connect_four/mcts.py
+++ connect_four/mcts.py
@@ -1,63 +1,72 @@
 from connect_four import board, player, node
 import sys, os
-import random, math
+import random
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-class Node:
-    def __init__(self, state, parent=None):
-        self.state = state
-        self.parent = parent
-        self.children = []
-        self.wins = 0
-        self.visits = 0
+# class Mcts:
+#     """Defines game tree for Connect 4
+#     Contains fuctions for Monte-Carlo Tree Search.
+#     """    
+#     # def __init__(self, board, stone, loops, last_node=None):
+#     #     """Inits by setting up the game tree.
 
-    def ucb1(self):
-        if self.visits == 0:
-            return float('inf')
-        exploitation = self.wins / self.visits
-        exploration = math.sqrt(math.log(self.parent.visits) / self.visits)
-        return exploitation + exploration
+#     #     Args:
+#     #         board (Board): 2-dimensional array of board
+#     #         stone (string): shape of marker 'O' or 'X' 
+#     #         loops (int): number of iterations for the loops
+#     #         last_node (Node, optional): last visited Node as selected. Defaults to None.
+#     #     """        
+#     #     print('inside function')
+#     #     if last_node != None:
+#     #         self.root = last_node
+#     #     else:
+#     #         self.root = node.Node(board.copy(), stone)
+#     #     print('enter')
+#     #     self.loops = loops
+#     #     self.initial_board = board
+#     #     self.stone = stone
+#     #     print('random')
 
-class MCTS:
-    def __init__(self, state):
-        self.root = Node(state)
+def random_move(input_board):
+    game_board = board.Board()
+    game_board.board = input_board
+    free_columns = game_board.get_free_columns()
+    if len(free_columns) == 0:
+        return -1
+    random_choice = random.choice(free_columns)
+    return random_choice
 
-    def selection(self):
-        node = self.root
-        while node.children:
-            node = max(node.children, key=lambda n: n.ucb1())
-        return node
+def better_move(input_board):
 
-    def expansion(self, node):
-        for move in node.state.get_legal_moves():
-            next_state = node.state.apply_move(move)
-            node.children.append(Node(next_state, node))
+    def winning_moves(temp_board, columns, stone):
+        win_moves = []
+        game_board = board.Board()
+        game_board.board = temp_board
 
-    def simulation(self, node):
-        state = node.state
-        while not state.is_game_over():
-            move = random.choice(state.get_legal_moves())
-            state = state.apply_move(move)
-        return state.winner
+        for col in columns:
+            row = game_board.get_free_row_index(col)
+            game_board.add_stone(row, col, stone)
+            if game_board.check_status(stone) == True:
+                win_moves.append(col)
+            game_board.add_stone(row, col, '_')
 
-    def backpropagation(self, node, outcome):
-        while node:
-            node.visits += 1
-            if outcome == node.state.get_current_player():
-                node.wins += 1
-            node = node.parent
+        return win_moves
 
-    def best_move(self):
-        return max(self.root.children, key=lambda n: n.visits).state.last_move
+    game_board = board.Board()
+    game_board.board = input_board
 
-    def run(self, simulations):
-        for i in range(simulations):
-            node = self.selection()
-            if not node.state.is_game_over():
-                if not node.children:
-                    self.expansion(node)
-                node = random.choice(node.children)
-                outcome = self.simulation(node)
-            else:
-                outcome = node.state.winner
-            self.backpropagation(node, outcome)
+    free_columns = game_board.get_free_columns()
+    if len(free_columns) == 0:
+        return -1
+
+    win_moves = winning_moves(game_board.board, free_columns, 'X')
+    lose_moves = winning_moves(game_board.board, free_columns, 'O')
+
+    if len(win_moves) > 0:
+        move = win_moves[0]
+    elif len(lose_moves) > 0:
+        move = lose_moves[0]
+    else:
+        move = random.choice(free_columns)
+
+    return move
