9a7e2e22d0f85a03f32b0fcc3e3b0202a53cffda
zfac131 <zfac131@live.rhul.ac.uk>
2023-02-25 18:03:05 +0000

    updated mcts.py and player.py

--- connect_four/mcts.py
+++ connect_four/mcts.py
@@ -1,72 +1,63 @@
 from connect_four import board, player, node
 import sys, os
-import random
+import random, math
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-# class Mcts:
-#     """Defines game tree for Connect 4
-#     Contains fuctions for Monte-Carlo Tree Search.
-#     """    
-#     # def __init__(self, board, stone, loops, last_node=None):
-#     #     """Inits by setting up the game tree.
+class Node:
+    def __init__(self, state, parent=None):
+        self.state = state
+        self.parent = parent
+        self.children = []
+        self.wins = 0
+        self.visits = 0
 
-#     #     Args:
-#     #         board (Board): 2-dimensional array of board
-#     #         stone (string): shape of marker 'O' or 'X' 
-#     #         loops (int): number of iterations for the loops
-#     #         last_node (Node, optional): last visited Node as selected. Defaults to None.
-#     #     """        
-#     #     print('inside function')
-#     #     if last_node != None:
-#     #         self.root = last_node
-#     #     else:
-#     #         self.root = node.Node(board.copy(), stone)
-#     #     print('enter')
-#     #     self.loops = loops
-#     #     self.initial_board = board
-#     #     self.stone = stone
-#     #     print('random')
-        
-def random_move(input_board):
-    game_board = board.Board()
-    game_board.board = input_board
-    free_columns = game_board.get_free_columns()
-    if len(free_columns) == 0:
-        return -1
-    random_choice = random.choice(free_columns)
-    return random_choice
+    def ucb1(self):
+        if self.visits == 0:
+            return float('inf')
+        exploitation = self.wins / self.visits
+        exploration = math.sqrt(math.log(self.parent.visits) / self.visits)
+        return exploitation + exploration
 
-def better_move(input_board):
+class MCTS:
+    def __init__(self, state):
+        self.root = Node(state)
 
-    def winning_moves(temp_board, columns, stone):
-        win_moves = []
-        game_board = board.Board()
-        game_board.board = temp_board
+    def selection(self):
+        node = self.root
+        while node.children:
+            node = max(node.children, key=lambda n: n.ucb1())
+        return node
 
-        for col in columns:
-            row = game_board.get_free_row_index(col)
-            game_board.add_stone(row, col, stone)
-            if game_board.check_status(stone) == True:
-                win_moves.append(col)
-            game_board.add_stone(row, col, '_')
+    def expansion(self, node):
+        for move in node.state.get_legal_moves():
+            next_state = node.state.apply_move(move)
+            node.children.append(Node(next_state, node))
 
-        return win_moves
+    def simulation(self, node):
+        state = node.state
+        while not state.is_game_over():
+            move = random.choice(state.get_legal_moves())
+            state = state.apply_move(move)
+        return state.winner
 
-    game_board = board.Board()
-    game_board.board = input_board
-    
-    free_columns = game_board.get_free_columns()
-    if len(free_columns) == 0:
-        return -1
-    
-    win_moves = winning_moves(game_board.board, free_columns, 'X')
-    lose_moves = winning_moves(game_board.board, free_columns, 'O')
+    def backpropagation(self, node, outcome):
+        while node:
+            node.visits += 1
+            if outcome == node.state.get_current_player():
+                node.wins += 1
+            node = node.parent
 
-    if len(win_moves) > 0:
-        move = win_moves[0]
-    elif len(lose_moves) > 0:
-        move = lose_moves[0]
-    else:
-        move = random.choice(free_columns)
-    
-    return move
+    def best_move(self):
+        return max(self.root.children, key=lambda n: n.visits).state.last_move
+
+    def run(self, simulations):
+        for i in range(simulations):
+            node = self.selection()
+            if not node.state.is_game_over():
+                if not node.children:
+                    self.expansion(node)
+                node = random.choice(node.children)
+                outcome = self.simulation(node)
+            else:
+                outcome = node.state.winner
+            self.backpropagation(node, outcome)
--- connect_four/player.py
+++ connect_four/player.py
@@ -1,25 +1,28 @@
-class Player:
-    """Defines the player for Connect 4.
-    """    
-    def __init__(self):
-        """Inits with 2 attributes; turn and stone.
-        Player 1 has turn value(1) and stone marker('O').
-        Player 2 has turn value(-1) and stone marker('X').
+from player import Player
 
-        Initially, it starts from player 1.
-        """        
-        self.turn = 1
-        self.stone = 'O'
+# Create two players, one using the MCTS algorithm and one using a simple heuristic
+player1 = Player(color=1, depth=None, algorithm='MCTS')
+player2 = Player(color=2, depth=3, algorithm='simple_heuristic')
 
-    def change_turn(self):
-        """Switches the turn to another player.
-        Changes the turn value and stone marker.
-         
-        """        
-        self.turn *= -1
-        if self.turn == 1:
-            self.stone = 'O'
-        else:
-            self.stone = 'X'
-
-                
+# Play the game with the two players
+board = [[0] * 7 for _ in range(6)]
+current_player = player1
+while True:
+    print_board(board)
+    if current_player is None:
+        print("It's a draw!")
+        break
+    if current_player.color == 1:
+        move = current_player.get_move(board)
+        print(f"Player 1 chooses column {move}")
+    else:
+        move = int(input("Player 2 choose a column (0-6): "))
+    if is_valid_move(board, move):
+        drop_piece(board, move, current_player.color)
+        if is_win(board, current_player.color):
+            print_board(board)
+            print(f"Player {current_player.color} wins!")
+            break
+        current_player = player2 if current_player == player1 else player1
+    else:
+        print("Invalid move. Please try again.")
